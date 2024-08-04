c8410bf7bd8cac3be6c030b13360c2fa72b5ed7f
Kyunghwan Kwon <k@mononn.com>
2024-08-04 17:11:22 +0900

    Initial commit

---
+++ hlw811x.c
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "hlw811x.h"
+#include "hlw811x_overrides.h"
+
+hlw811x_error_t hlw811x_init(void)
+{
+	return HLW811X_ERROR_NONE;
+}
---
+++ hlw811x.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef HLW811X_H
+#define HLW811X_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef enum {
+	HLW811X_ERROR_NONE		= 0,
+} hlw811x_error_t;
+
+hlw811x_error_t hlw811x_init(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* HLW811X_H */
